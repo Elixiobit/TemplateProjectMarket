cc696031feeca27cc42b76bfc51fdfe127b18e7c
Ilya Sushkov <pyh3@yandex.ru>
2020-02-16 01:22:24 +0300

    Completed 40%

--- js/main.js
+++ js/main.js
@@ -5,16 +5,18 @@ const products = [
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = 'product', price = 1500) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
+                <a href="#"><img src="http://placehold.it/200x250" /></a>
+                <h3 class="product-item__name">${title}</h3>
+                <p class="product-item__cost">${price}</p>
+                <button class="by-btn button">Добавить в корзину</button>
             </div>`;
 };
 
 const renderProducts = (list) => {
     const productList = list.map((item) => renderProduct(item.title, item.price));
+    // productList.join()
     document.querySelector('.products').innerHTML = productList;
 };
 
